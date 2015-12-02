177cdeafdd8ef2528ed91858589d4bb9370b34bd
root <root@ESIR-ABD-Bleas-Dubois.istic.univ-rennes1.fr>
2015-12-02 16:38:26 +0100

    Commit de l'exercice 3.1

---
+++ exo3.1/src/main/scala/SimpleApp.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+  def main(args: Array[String]) {
+	var a : Float =1
+	var b : Float =10
+	var n=500
+	var h : Float =(b-a)/n
+	val t1 = System.currentTimeMillis()
+	val conf = new SparkConf().setAppName("Spark Integrale")
+	val spark = new SparkContext(conf)
+	val slices = 50 
+	
+	var res = spark.parallelize(1 until n-1, slices).map { i =>
+		1/(a+i*h)
+	}.reduce(_ + _)
+	
+	res=res*h
+	
+	println("\n\n"+ res +" temps en ms =  " + (System.currentTimeMillis()-t1) +"\n")
+	spark.stop()	
+  }
+}

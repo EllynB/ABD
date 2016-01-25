a2deb84f6bd98f42fc1bdb858f9b06b7c3378845
root <root@ESIR-ABD-Bleas-Dubois.istic.univ-rennes1.fr>
2016-01-25 12:18:33 +0100

    Dernière version avec une seule machine

--- exo2.6/src/main/scala/SimpleApp.scala
+++ exo2.6/src/main/scala/SimpleApp.scala
@@ -4,7 +4,7 @@ import org.apache.spark.SparkConf
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "/home/ellyn/spark-1.5.2-bin-hadoop2.6/README.md" // Should be some file on your system
+    val logFile = "/home/ellyn/spark-1.6.0-bin-hadoop2.6/README.md" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
